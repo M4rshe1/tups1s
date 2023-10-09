1797cd5dcbb936eb0b03da6d9ffa0bfe920dbb60
COlin Heggli <colin.heggli@gmail.com>
2023-10-09 21:05:15 +0200

    ping tool

--- USB/Scripts/ping_tool/ping_stats.ps1
+++ USB/Scripts/ping_tool/ping_stats.ps1
@@ -37,15 +37,17 @@ function ping-device($dtp, $pd)
         $ping_result | Out-File -FilePath "output.txt" -Encoding UTF8
         $encoded_ping_result = [System.Text.Encoding]::UTF8.GetString([System.IO.File]::ReadAllBytes("output.txt"))
         $lines = $encoded_ping_result -split "`r`n"
-        $res_time = $lines[-2] -Split "Maximum = " | Select-Object -Last 1
-        $res_time = $res_time -Split "ms" | Select-Object -First 1
+        $all_results_time = $lines[-2] -Split "Maximum = " | Select-Object -Last 1
+        $all_results_time = $all_results_time -Split "ms" | Select-Object -First 1
 
         #        Write-Host $encoded_ping_result.split("Maximum = ")[1].split("ms")[0] -NoNewline
-        if (-not [int]::TryParse($res_time, [ref]$null))
+        if (-not [int]::TryParse($all_results_time, [ref]$null))
         {
             $ping_results["times"] += 0
-        } else {
-            $ping_results["times"] += [int]$res_time
+        }
+        else
+        {
+            $ping_results["times"] += [int]$all_results_time
         }
         $ping_results["timestamps"] += $ping_time
 
@@ -89,89 +91,146 @@ function ping-device($dtp, $pd)
     return $ping_results
 }
 
-function Show-Result($res, $device)
+function Show-Result($all_results, $device)
 {
-    $times = $res['times'] | Where-Object { $_ -gt 0 }
-
-    if ($times.Count -gt 0) {
-        $averageTime = [math]::round(($times | Measure-Object -Average).Average, 2)
-    } else {
-        $averageTime = 0
+    $longest = $all_results[0]
+    foreach ($i in $all_results)
+    {
+        if ($i["times"].Length -gt $longest["times"].Length)
+        {
+            $longest = $i
+        }
     }
-    Write-Host ""
-    Write-Host "  " -NoNewline
 
-    # Calculate the average time
-    $averageTime = [math]::round(($res['times'] | Measure-Object -Average).Average, 2)
+
+    #    $response_graph = @()
+    #    ForEach-Object $longest["times"] { $response_graph += "" }
     Write-Host "`nResponse Graph ((0 <10) <20 <30 <60 <120 <):"
 
-    for ($i = 0; $i -lt $res['times'].Count; $i++) {
-        if ($res['times'][$i] -eq 0) {
-            Write-Host -NoNewline "  0".PadRight(8) -ForegroundColor Red
+    for ($i = 0; $i -lt $longest["times"].Length; $i++)
+    {
+        foreach ($res in $all_results)
+        {
+            if ($res["times"][$i] -eq 0)
+            {
+                Write-Host "#".PadRight(7) -NoNewline -ForegroundColor Red
+                Write-Host ": $( $res["times"][$i] )ms".PadRight(8) -NoNewline
+            }
+            elseif ($res["times"][$i] -lt 10)
+            {
+                Write-Host "#".PadRight(7)  -NoNewline -ForegroundColor Green
+                Write-Host ": $( $res["times"][$i] )ms".PadRight(8) -NoNewline
+
+            }
+            elseif ($res["times"][$i] -lt 20)
+            {
+                Write-Host "#".PadRight(7)  -NoNewline -ForegroundColor Green
+                Write-Host ": $( $res["times"][$i] )ms".PadRight(8) -NoNewline
+            }
+            elseif ($res["times"][$i] -lt 30)
+            {
+                Write-Host "#".PadRight(7)  -NoNewline -ForegroundColor Green
+                Write-Host ": $( $res["times"][$i] )ms".PadRight(8) -NoNewline
+            }
+            elseif ($res["times"][$i] -lt 60)
+            {
+                Write-Host "#".PadRight(7)  -NoNewline -ForegroundColor Yellow
+                Write-Host ": $( $res["times"][$i] )ms".PadRight(8) -NoNewline
+            }
+            elseif ($res["times"][$i] -lt 120)
+            {
+                Write-Host "#".PadRight(7)  -NoNewline -ForegroundColor Red
+                Write-Host ": $( $res["times"][$i] )ms".PadRight(8) -NoNewline
+            }
+            else
+            {
+                Write-Host "#".PadRight(7)  -NoNewline -ForegroundColor Red
+                Write-Host ": $( $res["times"][$i] )ms".PadRight(8) -NoNewline
+            }
+            if ($all_results[-1] -ne $res)
+            {
+                Write-Host " | " -NoNewline
+            }
         }
-        elseif ($res['times'][$i] -lt 10) {
-            Write-Host -NoNewline "  #".PadRight(8) -ForegroundColor Green
-        }
-        elseif ($res['times'][$i] -lt 20) {
-            Write-Host -NoNewline "  ##".PadRight(8) -ForegroundColor Green
-        }
-        elseif ($res['times'][$i] -lt 30) {
-            Write-Host -NoNewline "  ###".PadRight(8) -ForegroundColor Green
-        }
-        elseif ($res['times'][$i] -lt 60) {
-            Write-Host -NoNewline "  ####".PadRight(8) -ForegroundColor Yellow
-        }
-        elseif ($res['times'][$i] -lt 120) {
-            Write-Host -NoNewline "  #####".PadRight(8) -ForegroundColor Yellow
-        }
-        else {
-            Write-Host -NoNewline "  ######".PadRight(8) -ForegroundColor Red
-        }
-        Write-Host " : " ($res['times'][$i]).ToString().PadLeft(4) "ms" -Separator ""
+        Write-Host ""
     }
 
-    Write-Host "`nResponse Graph in Real Time:`n(1000ms = 1 x #)`n   "
+    Write-Host "`nResponse Graph in Real Time:`n(1000ms = 1 x #)"
+    foreach ($res in $all_results)
+    {
+        foreach ($i in $res["times"])
+        {
+            if ($i -eq 0)
+            {
+                Write-Host "####" -NoNewline -ForegroundColor Red
+            }
+            elseif ($i -lt 30)
+            {
+                Write-Host "#" -NoNewline -ForegroundColor Green
+            }
+            elseif ($i -lt 60)
+            {
+                Write-Host "#" -NoNewline -ForegroundColor Yellow
+            }
+            elseif ($i -lt 120)
+            {
+                Write-Host "#" -NoNewline -ForegroundColor Yellow
+            }
+            elseif ($i -lt 1000)
+            {
+                Write-Host "#" -NoNewline -ForegroundColor Red
+            }
+            elseif ($i -lt 2000)
+            {
+                Write-Host "##" -NoNewline -ForegroundColor Red
+            }
+            elseif ($i -lt 3000)
+            {
+                Write-Host "###" -NoNewline -ForegroundColor Red
+            }
+            else
+            {
+                Write-Host "####" -NoNewline -ForegroundColor Yellow
+            }
+        }
+        Write-Host ""
+    }
+    Write-Host "`n"
 
-    # Loop through the response times
-    foreach ($i in $res["times"]) {
-        if ($i -lt 1) {
-            Write-Host -NoNewline -ForegroundColor Red "0000"
-        }
-        elseif ($i -lt 30) {
-            Write-Host -NoNewline -ForegroundColor Green "#"
-        }
-        elseif ($i -lt 120) {
-            Write-Host -NoNewline -ForegroundColor Yellow "#"
-        }
-        elseif ($i -lt 1000) {
-            Write-Host -NoNewline -ForegroundColor Red "#"
-        }
-        elseif ($i -lt 2000) {
-            Write-Host -NoNewline -ForegroundColor Red "##"
-        }
-        elseif ($i -lt 3000) {
-            Write-Host -NoNewline -ForegroundColor Red "###"
-        }
-        else {
-            Write-Host -NoNewline -ForegroundColor Yellow "####"
+
+    $summary = @(
+        "Requests   : "
+        "Responses  : "
+        "Lost       : "
+        "Loss       : "
+        "Min        : "
+        "Max        : "
+    )
+
+    foreach ($i in $all_results)
+    {
+        $summary[0] += $i["req"].ToString().PadRight(6)
+        $summary[1] += $i["res"].ToString().PadRight(6)
+        $summary[2] += $i["lost"].ToString().PadRight(6)
+        $summary[3] += ($i["loss"].ToString() + "% ").PadRight(6)
+        $summary[4] += ($i["min"].ToString() + "ms ").PadRight(6)
+        $summary[5] += ($i["max"].ToString() + "ms ").PadRight(6)
+        if ($all_results[-1] -ne $i)
+        {
+            $summary[0] += " : "
+            $summary[1] += " : "
+            $summary[2] += " : "
+            $summary[3] += " : "
+            $summary[4] += " : "
+            $summary[5] += " : "
         }
     }
-
-    Write-Host ""
-
-    Write-Host ""
     Write-Host "Ping results for $( $device ):"
-    Write-Host "  Total requests    : $( $res['req'] )"
-    Write-Host "  Received packets  : $( $res['res'] )"
-    Write-Host "  Lost packets      : $( $res['lost'] )"
-    Write-Host "  Packet loss       : $( $res['loss'] -f 'P2' )%"
-    Write-Host "  Minimum           : $( $res['min'] )ms"
-    Write-Host "  Average           : $( $averageTime -f 'F2' )ms"
-    Write-Host "  Maximum           : $( $res['max'] )ms"
-    Write-Host ""
-    Write-Host ""
+    foreach ($i in $summary)
+    {
+        Write-Host $i
+    }
 }
-
 
 
 
@@ -180,47 +239,53 @@ function Show-Result($res, $device)
 # ------------------------------------------------------- #
 #                          Main                           #
 # ------------------------------------------------------- #
+$all_ping_results = @()
+$device_to_ping = Read-Host "Enter device to ping (default: $DEFAULT_DEVICE)`n>> "
+if ($device_to_ping -eq "")
+{
+    $device_to_ping = $DEFAULT_DEVICE
+}
+$ping_duration = Read-Host "Enter ping duration Xm or Xs (default: $DEFAULT_PING_DURATION)`n>> "
+if ($ping_duration -eq "")
+{
+    $ping_duration = $DEFAULT_PING_DURATION
+}
+if ($ping_duration -match "^\d+s$")
+{
+    $ping_duration = $ping_duration -replace "s", ""
+    $ping_duration = [int]$ping_duration
+}
+elseif ($ping_duration -match "^\d+m$")
+{
+    $ping_duration = $ping_duration -replace "m", ""
+    $ping_duration = [int]$ping_duration * 60
+}
+elseif ([int]::TryParse($ping_duration, [ref]$null))
+{
+    $ping_duration = [int]$ping_duration
+}
+else
+{
+    Write-Host "Invalid ping duration: $ping_duration"
+    exit
+}
 
-    $device_to_ping = Read-Host "Enter device to ping (default: $DEFAULT_DEVICE)`n>> "
-    if ($device_to_ping -eq "")
-    {
-        $device_to_ping = $DEFAULT_DEVICE
-    }
-    $ping_duration = Read-Host "Enter ping duration Xm or Xs (default: $DEFAULT_PING_DURATION)`n>> "
-    if ($ping_duration -eq "")
-    {
-        $ping_duration = $DEFAULT_PING_DURATION
-    }
-    if ($ping_duration -match "^\d+s$")
-    {
-        $ping_duration = $ping_duration -replace "s", ""
-        $ping_duration = [int]$ping_duration
-    }
-    elseif ($ping_duration -match "^\d+m$")
-    {
-        $ping_duration = $ping_duration -replace "m", ""
-        $ping_duration = [int]$ping_duration * 60
-    }
-    elseif ([int]::TryParse($ping_duration, [ref]$null))
-    {
-        $ping_duration = [int]$ping_duration
-    }
-    else
-    {
-        Write-Host "Invalid ping duration: $ping_duration"
-        exit
-    }
-
+Clear-Host
+while ($true)
+{
+    $all_pings = ping-device $device_to_ping $ping_duration
+    $all_ping_results += $all_pings
     Clear-Host
-while ($true) {
-    $results = ping-device $device_to_ping $ping_duration
-    Clear-Host
-    Show-Result -res $results -device $device_to_ping
+    #    Write-Host $all_ping_results
+    Show-Result -all_results $all_ping_results -device $device_to_ping
     $redo = Read-Host "Redo? (y/n)`n>> "
     if ($redo -eq "n")
     {
+        $datetime = Get-Date -Format "yyyy.MM.dd_HH-mm-ss"
+        $all_ping_results | ConvertTo-Json | Out-File -FilePath "ping_results_$( $datetime ).json" -Encoding UTF8
         break
     }
+
     Clear-Host
 }
 
